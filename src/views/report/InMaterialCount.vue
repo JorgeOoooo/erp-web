68da8b7f31433047d1a488f40dd5967df04b9dcc
JorgeOooo <zhangyue@harmonycloud.cn>
2024-01-30 02:11:55 +0800

    feat: 财务

--- src/views/report/InMaterialCount.vue
+++ src/views/report/InMaterialCount.vue
@@ -1,328 +1,341 @@
-<!-- from 7 5  2 7 1  8 9 2 0 -->
 <template>
   <a-row :gutter="24">
     <a-col :md="24">
-      <a-card :style="cardStyle" :bordered="false">
-        <!-- 查询区域 -->
-        <div class="table-page-search-wrapper">
-          <a-form layout="inline" @keyup.enter.native="searchQuery">
-            <a-row :gutter="24">
-              <a-col :md="6" :sm="24">
-                <a-form-item
-                  label="往来单位"
-                  :labelCol="labelCol"
-                  :wrapperCol="wrapperCol"
+      <a-spin :spinning="loading">
+        <a-card :bordered="false">
+          <div class="select">
+            <div class="input">
+              <div class="input-item">
+                <span class="label">客户：</span>
+                <a-select
+                  placeholder="选择客户"
+                  showSearch
+                  v-model="supplierId"
+                  @change="changeValue"
+                  style="width: 140px"
                 >
-                  <a-select
-                    placeholder="选择往来单位"
-                    v-model="queryParam.organId"
-                    :dropdownMatchSelectWidth="false"
-                    showSearch
-                    allow-clear
-                    optionFilterProp="children"
+                  <a-select-option
+                    v-for="(item, index) in supList"
+                    :key="index"
+                    :value="item.id"
                   >
-                    <a-select-option
-                      v-for="(item, index) in organList"
-                      :key="index"
-                      :value="item.id"
-                    >
-                      {{ item.supplier }}
-                    </a-select-option>
-                  </a-select>
-                </a-form-item>
-              </a-col>
-              <a-col :md="6" :sm="24">
-                <a-form-item
-                  label="商品信息"
-                  :labelCol="labelCol"
-                  :wrapperCol="wrapperCol"
-                >
-                  <a-input
-                    placeholder="条码/名称/箱规/型号"
-                    v-model="queryParam.materialParam"
-                  ></a-input>
-                </a-form-item>
-              </a-col>
-              <a-col :md="6" :sm="24">
-                <a-form-item
-                  label="单据日期"
-                  :labelCol="labelCol"
-                  :wrapperCol="wrapperCol"
-                >
-                  <a-range-picker
-                    style="width: 100%"
-                    v-model="queryParam.createTimeRange"
-                    :default-value="defaultTimeStr"
-                    format="YYYY-MM-DD"
-                    :placeholder="['开始时间', '结束时间']"
-                    @change="onDateChange"
-                  />
-                </a-form-item>
-              </a-col>
-              <a-col :md="6" :sm="24">
-                <span
-                  style="float: left; overflow: hidden"
-                  class="table-page-search-submitButtons"
-                >
-                  <a-button type="primary" @click="searchQuery">查询</a-button>
-                  <a-button
-                    style="margin-left: 8px"
-                    v-print="'#reportPrint'"
-                    icon="printer"
-                    >打印</a-button
-                  >
-                  <a-button
-                    style="margin-left: 8px"
-                    @click="exportExcel"
-                    icon="download"
-                    >导出</a-button
-                  >
-                  <a @click="handleToggleSearch" style="margin-left: 8px">
-                    {{ toggleSearchStatus ? "收起" : "展开" }}
-                    <a-icon :type="toggleSearchStatus ? 'up' : 'down'" />
-                  </a>
-                </span>
-              </a-col>
-              <template v-if="toggleSearchStatus">
-                <a-col :md="6" :sm="24">
-                  <a-form-item
-                    label="仓库"
-                    :labelCol="labelCol"
-                    :wrapperCol="wrapperCol"
-                  >
-                    <a-select
-                      optionFilterProp="children"
-                      showSearch
-                      allow-clear
-                      style="width: 100%"
-                      placeholder="请选择仓库"
-                      v-model="queryParam.depotId"
-                    >
-                      <a-select-option
-                        v-for="(depot, index) in depotList"
-                        :value="depot.id"
-                        :key="index"
-                      >
-                        {{ depot.depotName }}
-                      </a-select-option>
-                    </a-select>
-                  </a-form-item>
-                </a-col>
+                    {{ item.supplier }}
+                  </a-select-option>
+                </a-select>
+              </div>
+              <div class="input-item">
+                <span class="label">月份：</span>
+                <a-date-picker
+                  v-if="dateTime"
+                  :default-value="dateTime"
+                  :format="dateFormat"
+                  :allowClear="false"
+                  @change="changeDateTime"
+                  style="width: 140px"
+                />
+              </div>
+              <div class="input-item">
+                <a-button type="primary" @click="changeValue">查询</a-button>
+                <!-- <a-button @click="exportView" style="margin-left: 12px"
+                  >导出预览</a-button
+                > -->
+              </div>
+            </div>
+          </div>
+          <div class="table" v-if="columns?.length">
+            <a-table
+              size="small"
+              bordered
+              :columns="columns"
+              :dataSource="dataSource"
+              :pagination="false"
+            >
+              <!-- <template slot="index" slot-scope="text, record, index">
+                {{ text ? text : index + 1 }}
               </template>
-            </a-row>
-          </a-form>
-        </div>
-        <!-- table区域-begin -->
-        <section ref="print" id="reportPrint">
-          <a-table
-            bordered
-            ref="table"
-            size="middle"
-            rowKey="id"
-            :columns="columns"
-            :dataSource="dataSource"
-            :components="handleDrag(columns)"
-            :pagination="false"
-            :scroll="scroll"
-            :loading="loading"
-            @change="handleTableChange"
-          >
-          </a-table>
-          <a-row :gutter="24" style="margin-top: 8px; text-align: right">
-            <a-col :md="24" :sm="24">
-              <a-pagination
-                @change="paginationChange"
-                @showSizeChange="paginationShowSizeChange"
-                size="small"
-                show-size-changer
-                :showQuickJumper="true"
-                :current="ipagination.current"
-                :page-size="ipagination.pageSize"
-                :page-size-options="ipagination.pageSizeOptions"
-                :total="ipagination.total"
-                :show-total="
-                  (total, range) =>
-                    `共 ${total - Math.ceil(total / ipagination.pageSize)} 条`
-                "
-              >
-                <template slot="buildOptionText" slot-scope="props">
-                  <span>{{ props.value - 1 }}条/页</span>
-                </template>
-              </a-pagination>
-            </a-col>
-          </a-row>
-        </section>
-        <!-- table区域-end -->
-      </a-card>
+              <template slot="supplier">
+                {{ "客户：" + (supplierName || "-") }}
+              </template>
+              <template slot="time">
+                {{ "日期：" + moment(dateTime).format(dateFormat) }}
+              </template> -->
+            </a-table>
+          </div>
+        </a-card>
+      </a-spin>
     </a-col>
   </a-row>
 </template>
 <script>
-import { JeecgListMixin } from "@/mixins/JeecgListMixin";
-import { getNowFormatYear, openDownloadDialog, sheet2blob } from "@/utils/util";
-import { getAction } from "@/api/manage";
-import { findBySelectOrgan } from "@/api/api";
-import JEllipsis from "@/components/jeecg/JEllipsis";
 import moment from "moment";
-import Vue from "vue";
+import { httpAction, getAction } from "@/api/manage";
+import { findBySelectSup } from "@/api/api";
 export default {
-  name: "InMaterialCount",
-  mixins: [JeecgListMixin],
-  components: {
-    JEllipsis,
-  },
+  name: "ouputTable",
+  components: {},
   data() {
     return {
-      labelCol: {
-        span: 5,
-      },
-      wrapperCol: {
-        span: 18,
-        offset: 1,
-      },
-      // 查询条件
-      queryParam: {
-        organId: "",
-        materialParam: "",
-        depotId: "",
-        beginTime: getNowFormatYear() + "-01-01",
-        endTime: moment().format("YYYY-MM-DD"),
-        type: "2",
-        roleType: Vue.ls.get("roleType"),
-      },
-      ipagination: {
-        pageSize: 11,
-        pageSizeOptions: ["11", "21", "31", "101", "201"],
-      },
-      dateFormat: "YYYY-MM-DD",
-      currentDay: moment().format("YYYY-MM-DD"),
-      defaultTimeStr: "",
-      organList: [],
-      depotList: [],
-      tabKey: "1",
-      // 表头
-      columns: [
-        {
-          title: "#",
-          dataIndex: "rowIndex",
-          width: 40,
-          align: "center",
-          customRender: function (t, r, index) {
-            return t !== "合计" ? parseInt(index) + 1 : t;
-          },
-        },
-        { title: "条码", dataIndex: "barCode", width: 120 },
-        { title: "名称", dataIndex: "mName", width: 120, ellipsis: true },
-        { title: "箱规", dataIndex: "standard", width: 100, ellipsis: true },
-        { title: "型号", dataIndex: "model", width: 100, ellipsis: true },
-        {
-          title: "类别",
-          dataIndex: "categoryName",
-          width: 120,
-          ellipsis: true,
-        },
-        {
-          title: "单位",
-          dataIndex: "materialUnit",
-          width: 120,
-          ellipsis: true,
-        },
-        {
-          title: "入库数量",
-          dataIndex: "numSum",
-          sorter: (a, b) => a.numSum - b.numSum,
-          width: 120,
-        },
-        {
-          title: "入库金额",
-          dataIndex: "priceSum",
-          sorter: (a, b) => a.priceSum - b.priceSum,
-          width: 120,
-        },
-      ],
-      url: {
-        list: "/depotHead/findInOutMaterialCount",
-      },
+      loading: false,
+      supList: [],
+      supplierId: undefined,
+
+      dateFormat: "YYYY-MM",
+      dateTime: [],
+
+      listTable: [],
     };
   },
-  created() {
-    this.getDepotData();
-    this.initSupplier();
-    this.defaultTimeStr = [
-      moment(getNowFormatYear() + "-01-01", this.dateFormat),
-      moment(this.currentDay, this.dateFormat),
-    ];
+  computed: {
+    dataSource() {
+      return [...this.listTable];
+    },
+    columns() {
+      if (!this.supplierId || !this.dateTime) return [];
+      const supplier = this.supList.find((v) => v.id == this.supplierId);
+      if (!supplier) return [];
+      const { packageTypeName, fullyManagedType } = supplier;
+      let col_left = [];
+      let col_right = [];
+      // todo: 根据不同类型展示
+      if (
+        packageTypeName == "半托" ||
+        (packageTypeName == "全托" && [2, 4].includes(fullyManagedType))
+      ) {
+        col_left.push({
+          title: supplier?.packageDeliverFee + "/配送",
+          align: "center",
+          dataIndex: "d1",
+          width: 80,
+        });
+        col_left.push({
+          title: supplier?.packageWarehouseFee + "/库房",
+          align: "center",
+          dataIndex: "d2",
+          width: 80,
+        });
+        col_right.push({
+          title: "入库数量",
+          align: "center",
+          dataIndex: "d2",
+          width: 80,
+        });
+        col_right.push({
+          title: "出库数量",
+          align: "center",
+          dataIndex: "d3",
+          width: 80,
+        });
+      }
+      if (packageTypeName == "全托" && [1, 2, 3].includes(fullyManagedType)) {
+        col_left.push({
+          title: supplier?.cubeDeliverFee + "/配送",
+          align: "center",
+          dataIndex: "d1",
+          width: 80,
+        });
+        col_left.push({
+          title: supplier?.cubeWarehouseFee + "/库房",
+          align: "center",
+          dataIndex: "d2",
+          width: 80,
+        });
+        col_right.push({
+          title: "入库立方",
+          align: "center",
+          dataIndex: "d2",
+          width: 80,
+        });
+        col_right.push({
+          title: "出库立方",
+          align: "center",
+          dataIndex: "d3",
+          width: 80,
+        });
+      }
+      return [
+        {
+          title: "日期",
+          children: [
+            {
+              title: "结束时间",
+              align: "center",
+              dataIndex: "endDate",
+              width: 100,
+            },
+            {
+              title: "结开始时间",
+              align: "center",
+              dataIndex: "startDate",
+              width: 100,
+            },
+          ],
+        },
+        {
+          title: "备注",
+          align: "center",
+          dataIndex: "remark",
+        },
+        {
+          title: "垫付费",
+          align: "center",
+          dataIndex: "d0",
+          width: 80,
+        },
+        {
+          title: "摘要",
+          children: [
+            ...col_left,
+            {
+              title: "费用合计",
+              align: "center",
+              dataIndex: "d5",
+              width: 80,
+            },
+            {
+              title: "天数",
+              align: "center",
+              dataIndex: "days",
+              width: 80,
+            },
+          ],
+        },
+        {
+          title: "余额",
+          align: "center",
+          dataIndex: "d7",
+          width: 80,
+        },
+        ...col_right,
+      ];
+    },
   },
   methods: {
     moment,
-    getQueryParams() {
-      let param = Object.assign({}, this.queryParam, this.isorter);
-      param.field = this.getQueryField();
-      param.currentPage = this.ipagination.current;
-      param.pageSize = this.ipagination.pageSize - 1;
-      return param;
-    },
-    onDateChange: function (value, dateString) {
-      console.log(dateString[0], dateString[1]);
-      this.queryParam.beginTime = dateString[0];
-      this.queryParam.endTime = dateString[1];
-    },
     initSupplier() {
       let that = this;
-      findBySelectOrgan({}).then((res) => {
+      findBySelectSup({}).then((res) => {
         if (res) {
-          that.organList = res;
+          that.supList = res;
         }
       });
     },
-    getDepotData() {
-      getAction("/depot/findDepotByCurrentUser").then((res) => {
-        if (res.code === 200) {
-          this.depotList = res.data;
-        } else {
-          this.$message.info(res.data);
-        }
-      });
+    changeDateTime(val) {
+      this.dateTime = val;
+      this.changeValue();
     },
-    searchQuery() {
-      if (this.queryParam.beginTime == "" || this.queryParam.endTime == "") {
-        this.$message.warning("请选择单据日期！");
-      } else {
-        this.loadData(1);
+    changeValue() {
+      if (!this.supplierId || !this.dateTime) return;
+      this.loading = true;
+      getAction("/documentHead/report/finance/range/date", {
+        supplierId: this.supplierId,
+        startDate: moment(this.dateTime).format(this.dateFormat) + "-26",
+        endDate:
+          moment(this.dateTime).add(1, "months").format(this.dateFormat) +
+          "-25",
+      })
+        .then(({ code, data }) => {
+          if (code == 200) {
+            this.listTable = data?.financeReportUnits || [];
+
+            let sum_1 = 0;
+            let sum_2 = 0;
+            let sum_3 = 0;
+            let sum_4 = 0;
+            let sum_5 = 0;
+            let sum_6 = 0;
+
+            let sum_7 = 0;
+
+            const days = this.listTable?.[1].days + 1;
+
+            this.listTable.forEach((v) => {
+              // sum_1 +=
+              // sum_2 +=
+              // sum_3 +=
+              // sum_4 +=
+              // sum_5 +=
+              // sum_6 +=
+              // sum_7 +=
+            });
+
+            const supplier = this.supList.find((v) => v.id == this.supplierId);
+            const { packageTypeName, fullyManagedType } = supplier;
+
+            if (
+              packageTypeName == "全托" &&
+              [1, 2, 3].includes(fullyManagedType)
+            ) {
+              // 立方
+              this.listTable.push({
+                endDate: this.listTable?.[this.listTable.length - 1]?.endDate,
+                startDate:
+                  moment(this.dateTime).add(2, "months").month() + 1 + "月",
+                remark: "库房费（立方）",
+                d5: (sum_3 + sum_4) * 60 * days,
+              });
+            }
+            if (
+              packageTypeName == "半托" ||
+              (packageTypeName == "全托" && [2, 4].includes(fullyManagedType))
+            ) {
+              // 包
+              this.listTable.push({
+                endDate: this.listTable?.[this.listTable.length - 1]?.endDate,
+                startDate:
+                  moment(this.dateTime).add(2, "months").month() + 1 + "月",
+                remark: "库房费（包结）",
+                d5: (sum_5 + sum_6) * 750,
+              });
+            }
+          }
+        })
+        .catch(() => {
+          this.listTable = [];
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    exportView() {
+      if (!this.supplierId || !this.dateTime) {
+        this.$message.warning("请选择客户和时间！");
+        return;
       }
+      this.$refs.exportRef.show();
     },
-    exportExcel() {
-      let aoa = [
-        [
-          "条码",
-          "名称",
-          "箱规",
-          "型号",
-          "类别",
-          "单位",
-          "入库数量",
-          "入库金额",
-        ],
-      ];
-      for (let i = 0; i < this.dataSource.length; i++) {
-        let ds = this.dataSource[i];
-        let item = [
-          ds.barCode,
-          ds.mName,
-          ds.standard,
-          ds.model,
-          ds.categoryName,
-          ds.materialUnit,
-          ds.numSum,
-          ds.priceSum,
-        ];
-        aoa.push(item);
-      }
-      openDownloadDialog(sheet2blob(aoa), "入库汇总");
-    },
+  },
+  created() {
+    this.dateTime = moment(moment().format(this.dateFormat), this.dateFormat);
+    this.initSupplier();
   },
 };
 </script>
-<style scoped>
-@import "~@assets/less/common.less";
+<style lang="less" scoped>
+.title {
+  font-size: 18px;
+  margin-bottom: 12px;
+  text-align: center;
+}
+.select {
+  margin-bottom: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .input-item {
+    display: inline-block;
+  }
+  .input-item + .input-item {
+    margin-left: 20px;
+  }
+  .label {
+    display: inline-block;
+    width: 80px;
+  }
+}
+.table {
+  margin-bottom: 12px;
+}
+.total-table {
+  width: 425px;
+}
 </style>
