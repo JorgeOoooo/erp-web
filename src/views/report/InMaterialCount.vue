3941cfc8249a43435a98ee0b5daabde9835ab1a2
JorgeOooo <zhangyue@harmonycloud.cn>
2024-02-08 15:27:15 +0800

    fix: 财务静态页面绘制

--- src/views/report/InMaterialCount.vue
+++ src/views/report/InMaterialCount.vue
@@ -25,7 +25,7 @@
               </div>
               <div class="input-item">
                 <span class="label">月份：</span>
-                <a-date-picker
+                <a-month-picker
                   v-if="dateTime"
                   :default-value="dateTime"
                   :format="dateFormat"
@@ -90,13 +90,78 @@ export default {
       return [...this.listTable];
     },
     columns() {
+      const renderContent = (value, row) => {
+        const obj = {
+          children: value,
+          attrs: {},
+        };
+        if (row.balance == "剩余") {
+          obj.attrs.colSpan = 0;
+        }
+        return obj;
+      };
+
+      const renderContent2 = (value, row) => {
+        const obj = {
+          children: value,
+          attrs: {},
+        };
+        if (row.balance == "剩余") {
+          obj.attrs.colSpan = 2;
+        }
+        return obj;
+      };
+
       if (!this.supplierId || !this.dateTime) return [];
       const supplier = this.supList.find((v) => v.id == this.supplierId);
       if (!supplier) return [];
       const { packageTypeName, fullyManagedType } = supplier;
       let col_left = [];
       let col_right = [];
-      // todo: 根据不同类型展示
+      if (packageTypeName == "全托" && [1, 2, 3].includes(fullyManagedType)) {
+        col_left.push({
+          title: supplier?.cubeDeliverFee + "/配送",
+          align: "center",
+          dataIndex: "cubeDeliverFee",
+          width: 80,
+        });
+        col_left.push({
+          title: supplier?.cubeWarehouseFee + "/库房",
+          align: "center",
+          dataIndex: "cubeWarehouseFee",
+          width: 80,
+        });
+
+        col_right.push({
+          title: "入库数量",
+          align: "center",
+          dataIndex: "inCubeNumber",
+          width: 80,
+          customRender: renderContent2,
+        });
+        col_right.push({
+          title: "出库数量",
+          align: "center",
+          dataIndex: "outCubeNumber",
+          width: 80,
+          customRender: renderContent,
+        });
+
+        col_right.push({
+          title: "入库立方",
+          align: "center",
+          dataIndex: "inCube",
+          width: 80,
+          customRender: renderContent2,
+        });
+        col_right.push({
+          title: "出库立方",
+          align: "center",
+          dataIndex: "outCube",
+          width: 80,
+          customRender: renderContent,
+        });
+      }
       if (
         packageTypeName == "半托" ||
         (packageTypeName == "全托" && [2, 4].includes(fullyManagedType))
@@ -104,52 +169,34 @@ export default {
         col_left.push({
           title: supplier?.packageDeliverFee + "/配送",
           align: "center",
-          dataIndex: "d1",
+          dataIndex: "packageDeliverFee",
           width: 80,
         });
         col_left.push({
           title: supplier?.packageWarehouseFee + "/库房",
           align: "center",
-          dataIndex: "d2",
+          dataIndex: "packageWarehouseFee",
           width: 80,
         });
+
         col_right.push({
-          title: "入库数量",
-          align: "center",
-          dataIndex: "d2",
-          width: 80,
-        });
-        col_right.push({
-          title: "出库数量",
-          align: "center",
-          dataIndex: "d3",
-          width: 80,
-        });
-      }
-      if (packageTypeName == "全托" && [1, 2, 3].includes(fullyManagedType)) {
-        col_left.push({
-          title: supplier?.cubeDeliverFee + "/配送",
-          align: "center",
-          dataIndex: "d1",
-          width: 80,
-        });
-        col_left.push({
-          title: supplier?.cubeWarehouseFee + "/库房",
-          align: "center",
-          dataIndex: "d2",
-          width: 80,
-        });
-        col_right.push({
-          title: "入库立方",
-          align: "center",
-          dataIndex: "d2",
-          width: 80,
-        });
-        col_right.push({
-          title: "出库立方",
-          align: "center",
-          dataIndex: "d3",
-          width: 80,
+          title: "750/包结",
+          children: [
+            {
+              title: "入库数量",
+              align: "center",
+              dataIndex: "inNumber",
+              width: 80,
+              customRender: renderContent2,
+            },
+            {
+              title: "出库数量",
+              align: "center",
+              dataIndex: "outNumber",
+              width: 80,
+              customRender: renderContent,
+            },
+          ],
         });
       }
       return [
@@ -176,9 +223,9 @@ export default {
           dataIndex: "remark",
         },
         {
-          title: "垫付费",
+          title: "额外费用",
           align: "center",
-          dataIndex: "d0",
+          dataIndex: "extraFee",
           width: 80,
         },
         {
@@ -188,21 +235,21 @@ export default {
             {
               title: "费用合计",
               align: "center",
-              dataIndex: "d5",
+              dataIndex: "totalFee",
               width: 80,
             },
             {
               title: "天数",
               align: "center",
               dataIndex: "days",
-              width: 80,
+              width: 60,
             },
           ],
         },
         {
           title: "余额",
           align: "center",
-          dataIndex: "d7",
+          dataIndex: "balance",
           width: 80,
         },
         ...col_right,
@@ -235,7 +282,33 @@ export default {
       })
         .then(({ code, data }) => {
           if (code == 200) {
-            this.listTable = data?.financeReportUnits || [];
+            let balance = 0;
+            this.listTable =
+              data?.financeReportUnits.map((v) => {
+                balance -= Number(v.totalFee);
+                return {
+                  ...v,
+                  extraFee: v.extraFee == 0 ? "" : v.extraFee,
+                  cubeDeliverFee: v.cubeDeliverFee == 0 ? "" : v.cubeDeliverFee,
+                  cubeWarehouseFee:
+                    v.cubeWarehouseFee == 0 ? "" : v.cubeWarehouseFee,
+                  packageDeliverFee:
+                    v.packageDeliverFee == 0 ? "" : v.packageDeliverFee,
+                  packageWarehouseFee:
+                    v.packageWarehouseFee == 0 ? "" : v.packageWarehouseFee,
+                  totalFee: v.totalFee == 0 ? "" : v.totalFee,
+                  // days: v.days == 0 ? "" : v.days,
+
+                  balance: balance == 0 ? "" : balance,
+
+                  inCubeNumber: v.inCubeNumber == 0 ? "" : v.inCubeNumber,
+                  outCubeNumber: v.outCubeNumber == 0 ? "" : v.outCubeNumber,
+                  inCube: v.inCube == 0 ? "" : v.inCube,
+                  outCube: v.outCube == 0 ? "" : v.outCube,
+                  inNumber: v.inNumber == 0 ? "" : v.inNumber,
+                  outNumber: v.outNumber == 0 ? "" : v.outNumber,
+                };
+              }) || [];
 
             let sum_1 = 0;
             let sum_2 = 0;
@@ -244,17 +317,15 @@ export default {
             let sum_5 = 0;
             let sum_6 = 0;
 
-            let sum_7 = 0;
-
             const days = this.listTable?.[1].days + 1;
 
             this.listTable.forEach((v) => {
-              // sum_1 +=
-              // sum_2 +=
-              // sum_3 +=
-              // sum_4 +=
-              // sum_5 +=
-              // sum_6 +=
+              sum_1 += Number(v.inCubeNumber);
+              sum_2 += Number(v.outCubeNumber);
+              sum_3 += Number(v.inCube);
+              sum_4 += Number(v.outCube);
+              sum_5 += Number(v.inNumber);
+              sum_6 += Number(v.outNumber);
               // sum_7 +=
             });
 
@@ -266,12 +337,17 @@ export default {
               [1, 2, 3].includes(fullyManagedType)
             ) {
               // 立方
+              const totalFee =
+                (Number(sum_3) + Number(sum_4)) * 60 * Number(days);
+              balance -= Number(totalFee);
               this.listTable.push({
                 endDate: this.listTable?.[this.listTable.length - 1]?.endDate,
                 startDate:
                   moment(this.dateTime).add(2, "months").month() + 1 + "月",
                 remark: "库房费（立方）",
-                d5: (sum_3 + sum_4) * 60 * days,
+                days: days,
+                totalFee: totalFee,
+                balance: balance,
               });
             }
             if (
@@ -279,14 +355,38 @@ export default {
               (packageTypeName == "全托" && [2, 4].includes(fullyManagedType))
             ) {
               // 包
+              const d =
+                packageTypeName == "全托" && fullyManagedType == 2 ? "" : days;
+              const totalFee = (Number(sum_5) + Number(sum_6)) * 750;
+              balance -= Number(totalFee);
               this.listTable.push({
                 endDate: this.listTable?.[this.listTable.length - 1]?.endDate,
                 startDate:
                   moment(this.dateTime).add(2, "months").month() + 1 + "月",
                 remark: "库房费（包结）",
-                d5: (sum_5 + sum_6) * 750,
+                days: d,
+                totalFee: totalFee,
+                balance: balance,
               });
             }
+
+            this.listTable.push({
+              endDate: "合计",
+              totalFee: -balance,
+              inCubeNumber: sum_1,
+              outCubeNumber: sum_2,
+              inCube: sum_3,
+              outCube: sum_4,
+              inNumber: sum_5,
+              outNumber: sum_6,
+            });
+
+            this.listTable.push({
+              balance: "剩余",
+              inCubeNumber: Number(sum_1) + Number(sum_2),
+              inCube: Number(sum_3) + Number(sum_4),
+              inNumber: Number(sum_5) + Number(sum_6),
+            });
           }
         })
         .catch(() => {
